87603daa2cff688d579ae017267b44674868200b
UzzielAriel <uzzielariel@gmail.com>
2024-07-17 12:45:00 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,37 @@
+# Snake
+
+A snake game written in Three.js
+
+## Tooling
+
+### Dependencies
+
+To install dependencies:
+
+```bash
+npm install
+```
+
+### Tests
+
+To run tests:
+
+```bash
+npm run test
+```
+
+### Linters
+
+To run linters:
+
+```bash
+npm run lint
+```
+
+### Formatters
+
+To run formatters:
+
+```bash
+npm run format
+```
--- src/services/events.ts
+++ src/services/events.ts
@@ -6,32 +6,16 @@ export function registerEvents(): void {
   document.addEventListener("keydown", (event) => {
     console.log(camera.rotation.x);
     if (
-      camera.rotation.x > -Math.PI / 4 - Math.PI / 8 &&
-      camera.rotation.x < -Math.PI / 4 + Math.PI / 8
+      camera.rotation.y > -Math.PI / 4 - Math.PI / 8 &&
+      camera.rotation.y < -Math.PI / 4 + Math.PI / 8
     ) {
       console.log("Camera facing: N");
     }
     if (
-      camera.rotation.x > -Math.PI * (3 / 4) - Math.PI / 8 &&
-      camera.rotation.x < -Math.PI * (3 / 4) + Math.PI / 8
+      camera.rotation.y > -Math.PI * (3 / 4) - Math.PI / 8 &&
+      camera.rotation.y < -Math.PI * (3 / 4) + Math.PI / 8
     ) {
       console.log("Camera facing: S");
-    }
-    if (
-      camera.rotation.x < 0 / 2 &&
-      camera.rotation.x > -Math.PI &&
-      camera.rotation.x < -Math.PI / 2 + Math.PI / 4 &&
-      camera.rotation.x > -Math.PI / 4 - Math.PI / 2
-    ) {
-      console.log("Camera facing: E");
-    }
-    if (
-      camera.rotation.x > 0 &&
-      camera.rotation.x < Math.PI &&
-      camera.rotation.x > Math.PI / 2 - Math.PI / 4 &&
-      camera.rotation.x < Math.PI / 4 + Math.PI / 2
-    ) {
-      console.log("Camera facing: W");
     }
     if (
       event.key == "d" &&
